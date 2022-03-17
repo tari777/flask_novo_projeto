b8982dad79023ff3a101cf91a1fe974e862e3a14
tari777 <matheusalisauska@gmail.com>
2022-03-17 07:53:00 -0300

    first commit

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import render_template, session, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    return render_template ('login.html', title='Pagina Login')
+    return render_template ('login.html')
+
 if __name__ == '__main__':
     app.run
